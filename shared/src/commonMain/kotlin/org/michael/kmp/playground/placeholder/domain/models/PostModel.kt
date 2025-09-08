2825aa6e2a646454911b150b5f3ee156c6a37bf9
Michael Noguera <michael.noguera.dev@gmail.com>
2025-09-08 15:49:36 -0600

    DELETE [Post Functionality]

--- shared/src/commonMain/kotlin/org/michael/kmp/playground/placeholder/domain/models/PostModel.kt
+++
@@ -1,8 +0,0 @@
-package org.michael.kmp.playground.placeholder.domain.models
-
-data class PostModel(
-    val userId: Int,
-    val id: Int,
-    val title: String,
-    val body: String
-)
--- shared/src/commonMain/kotlin/org/michael/kmp/playground/placeholder/domain/repositories/PostRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.michael.kmp.playground.placeholder.domain.repositories
-
-import org.michael.kmp.playground.core.network.NetworkResult
-import org.michael.kmp.playground.placeholder.domain.models.PostModel
-
-interface PostRepository {
-    suspend fun getPostById(id: Int): NetworkResult<PostModel>
-}
